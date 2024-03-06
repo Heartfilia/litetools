bf1c009792aad964c2ba96601062bcd8caa93fa9
Heartfilia <lodge6410@vip.qq.com>
2024-03-06 17:24:54 +0800

    备份parse当前操作

--- liteparser/jsonPath/jsontypes.go
+++ liteparser/jsonPath/jsontypes.go
@@ -1,5 +1,22 @@
 package jsonPath
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+)
+
+const (
+	_     = iota + '﮽'
+	dot   // .
+	left  // [
+	right // ]
+	line  // |
+	blank //
+)
+
 type Result struct {
 	resultString string
 	Int64        int64
@@ -16,4 +33,123 @@ func (r *Result) String() string {
 
 func (r *Result) SetString(str string) {
 	r.resultString = str
+}
+
+type sReplace string
+
+func replaceTo(str string, mode int) string {
+	if mode == 1 {
+		// 把不需要转义的字符串替换了 如 \.
+		str = strings.ReplaceAll(str, "\\.", string(dot))
+		str = strings.ReplaceAll(str, "\\[", string(left))
+		str = strings.ReplaceAll(str, "\\]", string(right))
+		str = strings.ReplaceAll(str, "\\|", string(line))
+		str = strings.ReplaceAll(str, "\\ ", string(blank))
+	} else {
+		str = strings.ReplaceAll(str, string(dot), "\\.")
+		str = strings.ReplaceAll(str, string(left), "\\[")
+		str = strings.ReplaceAll(str, string(right), "\\]")
+		str = strings.ReplaceAll(str, string(line), "\\|")
+		str = strings.ReplaceAll(str, string(blank), "\\ ")
+	}
+	return str
+}
+
+func SplitRule(rule string) []string {
+	// 替换特殊符号 如 \.   \[   \|
+	rule = replaceTo(rule, 1)
+	return strings.Split(rule, "|")
+}
+
+type resultCache struct {
+	Base    string         // 缓存当前处理的段的数据
+	BaseAny any            // 把用json提取的Base放到这里
+	Array   []any          // 如果下一段是 任意类型数组
+	Object  map[string]any // 如果下一段是任意类型的map对象
+}
+
+type regexRule struct {
+	OnlyArray *regexp.Regexp // [0]
+	OnlyKey   *regexp.Regexp // a
+	KeyArray  *regexp.Regexp // a[0]
+	// 不支持的格式 [0]b  --> not support
+}
+
+func (r *regexRule) isEmpty() bool {
+	// 判断是否为未初始化的数据结构
+	if r.OnlyArray == nil || r.OnlyKey == nil || r.KeyArray == nil {
+		return true
+	}
+	return false
+}
+func (r *regexRule) onlyArray(rule string) bool {
+	return r.OnlyArray.MatchString(rule)
+}
+
+func (r *regexRule) onlyKey(rule string) bool {
+	return r.OnlyKey.MatchString(rule)
+}
+
+func (r *regexRule) keyWithArray(rule string) bool {
+	return r.KeyArray.MatchString(rule)
+}
+
+var regRuleCache regexRule
+
+func (r *resultCache) parse(rule string) {
+	if regRuleCache.isEmpty() {
+		// 如果是没有初始化的情况下 需要初始化内部的参数
+		regRuleCache.OnlyArray = regexp.MustCompile("^\\[\\d+]$")
+		regRuleCache.OnlyKey = regexp.MustCompile("^[^[]+")
+		regRuleCache.KeyArray = regexp.MustCompile(".+\\[\\d+]$")
+	}
+
+	if regRuleCache.onlyArray(rule) {
+		// 如果当前目标key是 [0]  那么预期当前节点能获取到的key样式应该是 []any
+		fmt.Println("当前格式是 onlyArray   :", rule)
+	} else if regRuleCache.keyWithArray(rule) {
+		// 如果当前目标key是 a[0] 那么预期当前节点能获取到的样式应该是 map[string]any  需要拆分然后继续处理一次
+		fmt.Println("当前格式是 keyWithArray:", rule)
+
+	} else if regRuleCache.onlyKey(rule) {
+		// 如果当前目标key是 a    那么预期当前节点能获取到的样式应该是 map[string]any
+		fmt.Println("当前格式是 onlyKey     :", rule)
+	} else {
+		log.Fatal("m没有匹配到的格式是:", rule)
+	}
+
+}
+
+func transToObj(jsonString string) (any, error) {
+	var s any
+	err := json.Unmarshal([]byte(jsonString), &s)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func parseRule(jsonString, rule string) {
+	res, err := transToObj(jsonString)
+	if err != nil {
+		log.Fatal(err)
+		return // 这里东西要返回 后面处理
+	}
+
+	var js = resultCache{
+		Base:    jsonString,
+		BaseAny: res,
+	}
+
+	rule = replaceTo(rule, 0) // 恢复成正常的rule格式
+	eachBlock := strings.Split(rule, ".")
+	for _, each := range eachBlock {
+		js.parse(each)
+	}
+}
+
+func JudgeAndExtractEachRule(jsonString string, rules []string) {
+	for _, rule := range rules {
+		parseRule(jsonString, rule)
+	}
 }
--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"github.com/Heartfilia/litetools/litenet"
-)
+import "github.com/Heartfilia/litetools/liteparser"
 
 func main() {
+	_, err := liteparser.TryGet(`{"a":{"b":[1,2,3]}}`, "a.b[0].[1]")
+	_, err = liteparser.TryGet(`{"a":{"b":[1,2,3]}}`, "a.b[0].c")
+	if err != nil {
+		return
+	}
 	//fmt.Println(litedir.LiteDir())
 	//fmt.Println(litedir.FileJsonLoader("/Users/lodge/Library/Caches/lite-tools/browser/config.json"))
 	//fmt.Println(litenet.GetLAN())
 	//fmt.Println(litenet.GetWAN())
-	fmt.Println(litenet.GetUA())
-	fmt.Println(litenet.GetUA("pc"))
-	fmt.Println(litenet.GetUA("mobile"))
-	fmt.Println(litenet.GetUA("mac"))
-	fmt.Println(litenet.GetUA("ios", "android"))
-	fmt.Println(litenet.GetUA("chrome", "edge", "opera"))
-	fmt.Println(litenet.GetUA("chrome", "edge"))
+	//fmt.Println(litenet.GetUA())
+	//fmt.Println(litenet.GetUA("pc"))
+	//fmt.Println(litenet.GetUA("mobile"))
+	//fmt.Println(litenet.GetUA("mac"))
+	//fmt.Println(litenet.GetUA("ios", "android"))
+	//fmt.Println(litenet.GetUA("chrome", "edge", "opera"))
+	//fmt.Println(litenet.GetUA("chrome", "edge"))
 	//t := litetime.Time{
 	//Unit: "ms",
 	//Fmt: true,
