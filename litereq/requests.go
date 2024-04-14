5e327c7bd3b6818bc6402c1eabdea066f86406e5
Heartfilia <lodge6410@vip.qq.com>
2024-04-14 22:30:54 +0800

    先备份修改

--- litereq/requests.go
+++ litereq/requests.go
@@ -1,1 +1,26 @@
 package litereq
+
+import (
+	"github.com/Heartfilia/litetools/litereq/opt"
+	"net/url"
+)
+
+type Session struct {
+	MaxRetry int // max retry, default 1
+	request  *Request
+	response *Response
+}
+
+type Request struct {
+	URL     *url.URL
+	Ctx     *Context
+	Options *opt.Option
+}
+
+func NewSession() *Session {
+	return &Session{
+		MaxRetry: 1,
+		request:  &Request{},
+		response: &Response{},
+	}
+}
