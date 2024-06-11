d32a1b1c4b3454e5bf679add3fe548759c8ee788
Heartfilia <lodge6410@vip.qq.com>
2024-06-11 22:56:50 +0800

    拆分一下

--- litestring/lite_tag.go
+++ litestring/lite_tag.go
@@ -8,27 +8,27 @@ import (
 
 func D() string {
 	name, line := getFunName(2)
-	return ColorString(fmt.Sprintf("[ DEBUG  - %10s<%d>]", name, line), "blue")
+	return ColorString(fmt.Sprintf("[ DEBUG  - %10s]<%d>", name, line), "blue")
 }
 
 func I() string {
 	name, line := getFunName(2)
-	return ColorString(fmt.Sprintf("[  INFO  - %10s<%d>]", name, line), "cyan")
+	return ColorString(fmt.Sprintf("[  INFO  - %10s]<%d>", name, line), "cyan")
 }
 
 func S() string {
 	name, line := getFunName(2)
-	return ColorString(fmt.Sprintf("[SUCCESS - %10s<%d>]", name, line), "green")
+	return ColorString(fmt.Sprintf("[SUCCESS - %10s]<%d>", name, line), "green")
 }
 
 func W() string {
 	name, line := getFunName(2)
-	return ColorString(fmt.Sprintf("[  WARN  - %10s<%d>]", name, line), "yellow")
+	return ColorString(fmt.Sprintf("[  WARN  - %10s]<%d>", name, line), "yellow")
 }
 
 func E() string {
 	name, line := getFunName(2)
-	return ColorString(fmt.Sprintf("[ ERROR  - %10s<%d>]", name, line), "red")
+	return ColorString(fmt.Sprintf("[ ERROR  - %10s]<%d>", name, line), "red")
 }
 
 func getFunName(l int) (string, int) {
