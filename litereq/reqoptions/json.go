a50151d2fa15c4f7d06ebd9c4ffc26d72125c991
Heartfilia <lodge6410@vip.qq.com>
2024-11-01 16:05:55 +0800

    json

--- litereq/reqoptions/json.go
+++ litereq/reqoptions/json.go
@@ -6,6 +6,9 @@ import (
 	"log"
 )
 
+// SetJson : recommend struct
+//
+// 使用 map[string]any 或者 struct 结构的数据 推荐后面的
 func (o *Option) SetJson(object any) *Option {
 	if object != nil {
 		marshal, err := json.Marshal(object)
